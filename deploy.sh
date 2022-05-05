2943159d4b9f62e5f69544feb621b4aa7f7bb07f
Marcus Kram <github@1ns.at>
2022-05-05 14:47:46 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ composer install --no-dev
 npm install
 php artisan migrate --database portal --path database/migrations/portal
 php artisan migrate --database authentication --path database/migrations/authentication
+npm run production
