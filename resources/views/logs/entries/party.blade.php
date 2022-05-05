65bb586cbe0ec14fff7528eaeb69634e99a3c40b
Insax <m.kram@live.at>
2022-05-05 22:29:42 +0200

    Party  Logs

--- resources/views/logs/entries/party.blade.php
+++ resources/views/logs/entries/party.blade.php
@@ -12,7 +12,18 @@
     </a>
 @endif
 @switch($log->action)
-    @case('Created')
-    created the party {{ $log->group_name }}
+    @case('Invite')
+    invited Player <a
+        class="whitespace-no-wrap underline"
+        href="{{ route('account.view', ['account' => $log->invited_account_uid]) }}">
+        {{ $log->inviteeAccount->name }}
+    </a>
+    @if($log->clan_id)
+        in Family <a
+            class="whitespace-no-wrap underline"
+            href="{{ route('clan.view', ['clan' => $log->invited_player_clan_id]) }}">
+            {{ $log->inviteeClan->name }}
+        </a>
+    @endif to party {{ $log->group_name }}
     @break
 @endswitch
