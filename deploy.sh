ebaa362ab7ebf6907fd62c4cb58030a0bf234ea9
Insax <m.kram@live.at>
2022-05-05 14:41:13 +0200

    Allow migrations for seperate tables

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+composer install --no-dev
+npm install
+php artisan migrate --database portal --path database/migrations/portal
+php artisan migrate --database authentication --path database/migrations/authentication
