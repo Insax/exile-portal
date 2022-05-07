50fd8bfbf43adcf9414c78dab8d5cd8b3a78e1af
Insax <m.kram@live.at>
2022-05-07 21:08:47 +0200

    Add Missing Closing Bracket

--- resources/views/livewire/display-account.blade.php
+++ resources/views/livewire/display-account.blade.php
@@ -1,4 +1,4 @@
 @php /** @var \App\Models\Account $account */ @endphp
 <div>
-    <a class="whitespace-no-wrap underline" href="{{ route('account.view', ['account' => $account->uid]) }}">{{ $account->name }}</a> ({{ $account->uid }}
+    <a class="whitespace-no-wrap underline" href="{{ route('account.view', ['account' => $account->uid]) }}">{{ $account->name }}</a> ({{ $account->uid }})
 </div>
