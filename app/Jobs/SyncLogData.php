85fd0ac70c4668f5b89e2dbc9c9886ae5f3395c9
Insax <m.kram@live.at>
2022-05-09 00:01:46 +0200

    Fix Parsing

--- app/Jobs/SyncLogData.php
+++ app/Jobs/SyncLogData.php
@@ -678,6 +678,9 @@ class SyncLogData implements ShouldQueue
         }
 
         foreach ($playerKillLogs as $log) {
+            if($log->killer_id == '' || $log->victim_id == '')
+                continue;
+
             Clan::findOrCreateDummy($log->killer_clan_id);
             Clan::findOrCreateDummy($log->victim_clan_id);
             $loggable = PlayerKillLog::create([
