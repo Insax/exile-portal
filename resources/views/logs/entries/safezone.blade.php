48e3595fa5aa498216a4242380952195b9e0c236
Marcus Kram <github@1ns.at>
2022-06-13 15:07:43 +0200

    Update safezone.blade.php

--- resources/views/logs/entries/safezone.blade.php
+++ resources/views/logs/entries/safezone.blade.php
@@ -11,14 +11,19 @@ at Position {{ $log->player_pos }}
 @switch($log->action)
     @case('VehicleKick')
     got kicked out of Vehicle {{ __($log->vehicle) }} owned by
-    @include('livewire.display-account', ['uid' => $log->vehicle_owner_uid, 'name' => $log->ownerAccount->name])
-    @if($log->vehicle_owner_clan_id)
+    @if($log->vehicle_owner_uid)
+        @include('livewire.display-account', ['uid' => $log->vehicle_owner_uid, 'name' => $log->ownerAccount->name])
+        @if($log->vehicle_owner_clan_id)
         in Family <a
             class="whitespace-no-wrap underline"
             href="{{ route('clan.view', ['clan' => $log->vehicle_owner_clan_id]) }}">
             {{ $log->ownerClan->name }}
         </a> at Position {{ $log->vehicle_pos }}
     @endif
+    @else
+        Unknown Player
+    @endif
+    
     @break
     @case('Entered')
     {{ strtolower($log->action) }} Safezone in a {{ __($log->vehicle) }} on Position {{ $log->player_pos }}
