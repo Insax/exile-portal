130f361cbc6d56a1ddef403f3efbfdd47a442f12
Marcus Kram <github@1ns.at>
2022-08-28 21:51:18 +0200

    Update SyncLogData.php

--- app/Jobs/SyncLogData.php
+++ app/Jobs/SyncLogData.php
@@ -850,7 +850,7 @@ class SyncLogData implements ShouldQueue
         }
 
         foreach ($territoryLogs as $log) {
-            if((empty($log->target_id) && $log->target_id != null) || empty($log->player_id))
+            if($log->target_id == '' || empty($log->player_id))
                 continue;
 
             Clan::findOrCreateDummy($log->clan_id);
