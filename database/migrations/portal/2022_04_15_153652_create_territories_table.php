986d996bf2bedcd0b5177c99cd11c3a9c6b180bc
Marcus Kram <github@1ns.at>
2023-04-06 18:53:38 +0200

    Update 2022_04_15_153652_create_territories_table.php

--- database/migrations/portal/2022_04_15_153652_create_territories_table.php
+++ database/migrations/portal/2022_04_15_153652_create_territories_table.php
@@ -34,6 +34,7 @@ return new class extends Migration
             $table->unsignedInteger('esm_payment_counter')->nullable();
             $table->dateTime('deleted_at')->nullable();
             $table->string('territory_permissions', 300);
+            $table->integer('bought_slots');
             $table->dateTime('last_updated_at');
             $table->timestamps();
             $table->softDeletes('trashed_at');
