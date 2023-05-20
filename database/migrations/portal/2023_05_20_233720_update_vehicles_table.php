31ebe4a97d3c5e46d7d1dc462074ef62d93a9904
Marcus Kram <github@1ns.at>
2023-05-20 02:31:13 +0200

    Create 2023_05_20_233720_update_vehicles_table.php

---
+++ database/migrations/portal/2023_05_20_233720_update_vehicles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $connection = 'portal';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+           $table->string('vehicle_ammo')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            Schema::dropColumn('vehicle_ammo');
+        });
+    }
+};
