19796b2594612b87bc5683b851e322f44dc79c5f
Insax <github@1ns.at>
2024-04-04 03:04:13 +0200

    Fix

--- app/Jobs/SyncDatabaseData.php
+++ app/Jobs/SyncDatabaseData.php
@@ -103,7 +103,7 @@ class SyncDatabaseData implements ShouldQueue
                 'esm_reward' => $account->esm_reward,
                 'exp_total' => $account->exp_total,
                 'first_connect_at' => $account->first_connect_at,
-                'forum_reward' => 'false',
+                'forum_reward' => 0,
                 'friend_last_reset_at' => $account->friend_last_reset_at,
                 'last_reward_at' => $account->last_reward_at,
                 'marxet_locker' => $account->marxet_locker,
