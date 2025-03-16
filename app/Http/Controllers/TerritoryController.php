43347c62e15ba703d3f1c7790a79a6b8ca01ca2b
Marcus Kram <github@1ns.at>
2025-03-16 10:08:17 +0100

    Update TerritoryController.php

--- app/Http/Controllers/TerritoryController.php
+++ app/Http/Controllers/TerritoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Territory;
 use Spatie\Activitylog\Models\Activity;
+use Carbon\Carbon;
 
 class TerritoryController extends Controller
 {
@@ -12,13 +13,20 @@ class TerritoryController extends Controller
         return view('territory.list');
     }
 
-    public function viewTerritory(Territory $territory)
+    public function viewTerritory($territory)
     {
-        $activities = Activity::forSubject($territory)->orderBy('created_at', 'ASC')->get();
-        return view('territory.view', [
-            'territory' => $territory,
-            'activities' => $activities
-        ]);
+        $terr = Territory::withTrashed()->whereId($territory)->limit(1);
+        if($terr) {
+            if($terr->deleted_at <= Carbon::now()->subDays(14))
+                abort(404);
+            
+            $activities = Activity::forSubject($territory)->orderBy('created_at', 'ASC')->get();
+            return view('territory.view', [
+                'territory' => $terr,
+                'activities' => $activities
+            ]);
+        }
+        abort(404)
     }
 }
 
